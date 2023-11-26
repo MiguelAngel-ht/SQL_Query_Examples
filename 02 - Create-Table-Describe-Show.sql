23a7cd22c3ed81039ae4aa1c76a642da27bc20c7
Miguel Ángel <78463148+MiguelAngel-ht@users.noreply.github.com>
2023-11-26 17:11:53 -0600

    Update 02 - Create-Table-Describe-Show.sql

--- 02 - Create-Table-Describe-Show.sql
+++ 02 - Create-Table-Describe-Show.sql
@@ -1,20 +1,20 @@
 use prueba2;
 
--- crear tabla 
-create table tabla1(
-			id int auto_increment,		-- entero que crece siempre
-            nombre varchar(250),
-            cartera float,
-            comentarios text,
-            fecha date,
-            primary key(id)				-- identificador único
+-- create a table
+create table table1(
+			id 		int 	auto_increment, 		-- integer that always increments
+			name		varchar(250),				-- string variable with 250 characters
+			wallet 		float,		
+			comments 	text,
+			date 		date,					-- to datetime variables 'YYYY-MM-DD'
+			primary key(id) -- unique identifier
 );
 
--- descripcion de tabla sql
-describe tabla1;
+-- SQL table description  (columns and data types)
+describe table1;
 
--- mostrar todas las tablas de tu base de datos
+-- show all tables in your database
 show tables;
 
--- mostrar todas tus bases de datos
+-- show all your databases
 show databases;
