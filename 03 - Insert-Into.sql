430b448182f8c6d4d88ca60643f0f02e9fef7c75
Miguel Ángel <78463148+MiguelAngel-ht@users.noreply.github.com>
2023-11-26 17:12:20 -0600

    Add files via upload

---
+++ 03 - Insert-Into.sql
@@ -0,0 +1,16 @@
+use prueba2;
+
+-- para copiar los campos
+describe tabla1;
+
+-- añadir un registro a la tabla1
+insert into tabla1 (nombre,
+					cartera,
+                    comentarios,
+                    fecha)
+			values ('Miguel',
+					55000.4,
+                    'Buen cliente',
+                    '2023-05-11');
+
+
