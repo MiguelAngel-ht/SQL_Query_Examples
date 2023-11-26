aceadbe570ee6a10918709ca57434354e63b0461
Miguel Ángel <78463148+MiguelAngel-ht@users.noreply.github.com>
2023-11-26 17:06:39 -0600

    Add files via upload

---
+++ 01 - Create-Use-Drop.sql
@@ -0,0 +1,11 @@
+
+-- crear base de datos
+-- utf8 para palabras en México (con la ñ)
+create database prueba1;
+
+-- para cambiar de contexto
+-- utilizamos 'use'
+use prueba2;
+
+-- borrar base de datos
+drop database prueba1;
